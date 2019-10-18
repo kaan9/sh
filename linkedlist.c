01cc4c0cfd76bef33d577b9661a92d734bb67dbc
Belinda Liu <belindaliu@huntsman-ve703-11398.apn.wlan.private.upenn.edu>
2019-10-18 08:28:50 -0400

    updated list for null and empty

--- linkedlist.c
+++ linkedlist.c
@@ -4,8 +4,16 @@
  */
 #include "linkedlist.h"
 
+DEQUE * make_empty_list(int (*deleter)(void *)) {
+    DEQUE * d = malloc(sizeof(DEQUE));
+    if (!d) return NULL;
+    d->dealloc = deleter;
+    d->tail = d->head = NULL;
+    d->size = 0;
+    return d;
+}
+
 DEQUE * make_list(void * val, int (*deleter)(void *)) {
-    if (!val) return NULL;  //NULL values not allowed in DEQUE
     DEQUE * d = malloc(sizeof(DEQUE));
     if (!d) return NULL;  //if malloc fails, return NULL
     d->dealloc = deleter;
@@ -16,12 +24,16 @@ DEQUE * make_list(void * val, int (*deleter)(void *)) {
     }//if malloc fails, return NULL
     d->head->val  = val;
     d->head->prev = d->head->next = NULL;
+    d->size = 1;
     return d;
 }
 
 int delete_list(DEQUE * d) {
     if (!d) return -1;
-
+    if (d->size == 0) {
+        free(d);
+        return 0;
+    }
     int success        = 0;
     struct node * curr = d->head;
     while (curr) {
@@ -42,54 +54,70 @@ void * list_back(DEQUE * d) {
 }
 
 DEQUE * push_back(DEQUE * d, void * val) {
-    if (!val || !d) return d;  //null values not allowed in DEQUE
-
+    if (!d) return NULL;  //check that deque is not null
     struct node * tmp = malloc(sizeof(struct node));
     if (!tmp) return NULL;
     tmp->val          = val;
     tmp->prev         = d->tail;
     tmp->next = NULL;
-    d->tail = d->tail->next = tmp;
+    if (d->size == 0) {
+        d->head = tmp;
+        d->tail = tmp;
+    } else {
+        d->tail = d->tail->next = tmp;
+    }
+    d->size++;
     return d;
 }
 
 DEQUE * push_front(DEQUE * d, void * val) {
-    if (!val || !d) return d;  //null values not allowed in DEQUE
-
+    if (!d) return NULL;  //check that deque is not null
     struct node * tmp = malloc(sizeof(struct node));
     if (!tmp) return NULL;
     tmp->val          = val;
     tmp->next         = d->head;
     tmp->prev = NULL;
-    d->head = d->head->prev = tmp;
+    if (d->size == 0) {
+        d->head = tmp;
+        d->tail = tmp;
+    } else {
+        d->head = d->head->prev = tmp;
+    }
+    d->size++;
     return d;
 }
 
 void * pop_back(DEQUE * d) {
     if (!d) return NULL;       //invalid input
-
+    if (d->size == 0) {
+        return NULL; //nothing to pop
+    }
     struct node * tmp = d->tail;
-
     d->tail    = d->tail->prev;
-    void * val = tmp->val;
+    void * ret = tmp->val;
     free(tmp);
-
-    if (!d->tail) free(d);  //if no elements left in DEQUE, destroy DEQUE
-    else d->tail->next = NULL;
-    return val;
+    if (d->size == 1) {
+        d->head = NULL;
+    } else {
+        d->tail->next = NULL;
+    }
+    d->size--;
+    return ret;
 }
 
 void * pop_front(DEQUE * d) {
     if (!d) return NULL;       //invalid input
-
     struct node * tmp = d->head;
     d->head           = d->head->next;
-    void * val        = tmp->val;
+    void * ret        = tmp->val;
     free(tmp);
-
-    if (!d->head) free(d);  //if no elements left in DEQUE, destroy DEQUE
-    else d->head->prev = NULL;
-    return val;
+    if (d->size == 1) {
+        d->tail = NULL;
+    } else {
+        d->head->prev = NULL;
+    }
+    d->size--;
+    return ret;
 }
 
 struct node * traverse(struct node * n, size_t i) {
@@ -108,12 +136,19 @@ void * extract_node(DEQUE * d, struct node * n) {
     if (!d || !n || !d->head || !d->tail) return NULL;
     if (d->head == n) return pop_front(d);
     if (d->tail == n) return pop_back(d);
-
-    void * val = n->val;
-    struct node * prev = n->prev;
-    struct node * next = n->next;
-    free(n);
-    prev->next = next;
-    next->prev = prev;
-    return val;
+    if (d->size == 1) {
+        void * ret = n->val;
+        d->head = d->tail = NULL;
+        d->size--;
+        return ret;
+    } else {
+        void * ret = n->val;
+        struct node * prev = n->prev;
+        struct node * next = n->next;
+        free(n);
+        prev->next = next;
+        next->prev = prev;
+        d->size--;
+        return ret;
+    }
 }
